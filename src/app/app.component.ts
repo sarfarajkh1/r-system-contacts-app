830eada426fa896ceb633c297e670b9ad16ab207
Sarfaraj Khatri <sarfarajkhatri7892@gmail.com>
2024-11-13 22:55:09 +0530

    save and delete componenet added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContactListComponent } from "./contact-list/contact-list.component";
+import { SaveContactComponent } from "./save-contact/save-contact.component";
+import { DeleteContactComponent } from "./delete-contact/delete-contact.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContactListComponent],
+  imports: [RouterOutlet, ContactListComponent, SaveContactComponent, DeleteContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
