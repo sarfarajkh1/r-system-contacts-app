1ae1153c0ec5c9843e112dd6ec5de3e9f092318c
Sarfaraj Khatri <sarfarajkhatri7892@gmail.com>
2024-11-15 18:12:14 +0530

    dynamic data for list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { ContactListComponent } from "./contact-list/contact-list.component";
 import { SaveContactComponent } from "./save-contact/save-contact.component";
 import { DeleteContactComponent } from "./delete-contact/delete-contact.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, ContactListComponent, SaveContactComponent, DeleteContactComponent],
+  providers:[
+    HttpClient
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnChanges, SimpleChanges } from '@angular/core';
+import { ContactService } from '../services/contact.service';
+import { Contact } from '../models/contact';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [],
-  templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.css'
+  imports: [NgFor],
+  templateUrl: './contact-list.component.html'
 })
-export class ContactListComponent {
+export class ContactListComponent implements AfterViewInit {
 
+  constructor(private contactService: ContactService){  }
+  ngAfterViewInit(): void {
+    this.getContacts();
+  }
+  contacts: Contact[] | undefined;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    //this.getContacts();
+  }
+
+  getContacts(){
+    this.contactService.getContacts()
+    .subscribe({
+      next:(response: Contact[]) => {
+        this.contacts = response;
+      }
+    })  
+  }
 }
